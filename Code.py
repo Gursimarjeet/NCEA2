f238e17b1119088e8343e3ac0ea44a4f7e5630b6
Gursimarjeet <24492@burnside.school.nz>
2026-05-29 00:36:13 +1200

    Added user interface and some comments

--- Code.py
+++ Code.py
@@ -1,9 +1,34 @@
+#Docstring = Gursimrjeet Singh = Graphics Card Information
+#imports
 import sqlite3
+#contants and variables
+DATABASE = 'Graphics'
 
-db = sqlite3.connect('Graphics')
-cursor = db.cursor()
-sql = "SELECT * FROM GraphicCardInfo;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+
+#functions
+def print_all_Manufacturer():
+    '''This function will print all the manufacturers in the database'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM GraphicCardInfo;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results and print them
+    print(f"Manufacturer        GPU_Model                Vram_Size    Price(NZD)   Clock_Speed")
+    for result in results:
+        print(f"{result[1]:<20}{result[2]:<25}{result[3]:<15}{result[4]:<10}{result[5]}")
+    #loop finish
+    db.close()
+
+#main code
+while True:
+    User = input("\nWhat would you like to do? \n1. print all manufacturers \n2. exit")
+    if User == '1':
+        print_all_Manufacturer()
+    if User == '2':
+        print("Thank You")
+        break
+    else:
+        print("Invalid input, please try again.")
+        break
+
